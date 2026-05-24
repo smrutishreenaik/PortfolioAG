6a0508a9ff1b9d1ce5f7a6ae86d06256128e254f
SmrutishreeNaik <smrutishree.naik123@gmail.com>
2026-05-24 06:06:42 +0530

    updated games section ui

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -8,6 +8,7 @@ const GAMES_LIST = [
     title: "Snake Game",
     description: "The classic Nokia game. Eat food to grow, but don't bite yourself!",
     tags: ["React State", "Game Loop", "Logic"],
+    image: "/snake-bg.png",
   },
 ];
 
@@ -18,28 +19,29 @@ const Games: React.FC = () => {
 
   return (
     <div className={styles.pageWrapper}>
-      <div className={styles.pageHeader}>
-        <h1 className={styles.pageTitle}>Games</h1>
-        <p className={styles.pageSubtitle}>
-          A collection of my interactive games and experiments.
-        </p>
-      </div>
-
       <div className={styles.contentContainer}>
         <div className={styles.gamesGrid}>
           {GAMES_LIST.map((game) => (
-            <Link key={game.id} to={`/games/${game.id}`} className={styles.gameCard}>
-              <div className={styles.cardHeader}>
-                <h3>{game.title}</h3>
-              </div>
-              <p className={styles.cardDesc}>{game.description}</p>
-              <div className={styles.cardTags}>
-                {game.tags.map((tag) => (
-                  <span key={tag} className={styles.tag}>{tag}</span>
-                ))}
-              </div>
-              <div className={styles.cardFooter}>
-                Play Now &rarr;
+            <Link 
+              key={game.id} 
+              to={`/games/${game.id}`} 
+              className={styles.gameCard}
+              style={game.image ? { backgroundImage: `url(${game.image})` } : {}}
+            >
+              <div className={styles.cardOverlay}></div>
+              <div className={styles.cardContent}>
+                <div className={styles.cardHeader}>
+                  <h3>{game.title}</h3>
+                </div>
+                <p className={styles.cardDesc}>{game.description}</p>
+                <div className={styles.cardTags}>
+                  {game.tags.map((tag) => (
+                    <span key={tag} className={styles.tag}>{tag}</span>
+                  ))}
+                </div>
+                <div className={styles.cardFooter}>
+                  Play Now &rarr;
+                </div>
               </div>
             </Link>
           ))}
